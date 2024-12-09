401cf7f2b3ed0384d83fff11b2c925fd22fd37af
Zsoltcsizmadia <163414210+Zsoltcsizmadia@users.noreply.github.com>
2024-12-09 13:58:01 +0100

    Add project files.

---
+++ KeszthelySprint/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace KeszthelySprint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.
+            List<Versenyző> versenyzok = new();
+            foreach (var sor in File.ReadAllLines("eredmenyek.txt"))
+            {
+                versenyzok.Add(new Versenyző(sor));
+            }
+            Console.WriteLine($"2. feladat: A versenyt {versenyzok.Count} fő versenyző fejezte be ");
+
+
+            //3.
+            var junior = 0;
+
+            foreach (var v in versenyzok)
+            {
+                if (v.ÉletKategória == "elit junior")
+                {
+                    junior++;
+                }
+            }
+            Console.WriteLine($"3. feladat: Versenyzők száma az \"elit junior\" kategóriában: {junior} fő");
+
+
+            //4.
+            double ossz_eletkor = 0;
+            foreach (var v in versenyzok)
+            {
+                ossz_eletkor += DateTime.Now.Year - v.SzületésiÉv;
+            }
+            Console.WriteLine($"4. feladat: Átlag életkor: {Math.Round(ossz_eletkor / versenyzok.Count, 1)} év");
+        }
+    }
+}
---
+++ KeszthelySprint/Versenyző.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeszthelySprint
+{
+    internal class Versenyző
+    {
+        public string Név { get; set; }
+        public int SzületésiÉv { get; set; }
+        public int Rajtszám { get; set; }
+        public string Nem { get; set; }
+        public string ÉletKategória { get; set; }
+        public string ÚszásIdeje { get; set; }
+        public string DepóbanTöltöttIdő { get; set; }
+        public string KerékpározásIdeje { get; set; }
+        public string MásodikDepóIdő { get; set; }
+        public string FutásIdeje { get; set; }
+
+        public Versenyző(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            Név = s[0];
+            SzületésiÉv = int.Parse(s[1]);
+            Rajtszám = int.Parse(s[2]);
+            Nem = s[3];
+            ÉletKategória = s[4];
+            ÚszásIdeje = s[5];
+            DepóbanTöltöttIdő = s[6];
+            KerékpározásIdeje = s[7];
+            MásodikDepóIdő = s[8];
+            FutásIdeje = s[9];
+        }
+
+    }
+}
